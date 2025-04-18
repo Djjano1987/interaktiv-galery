0762193bf6d6cce642372b4e94161037223e1fab
K Jani <k.janika87@gmail.com>
2025-04-18 05:16:12 +0200

    Fix: clean node_modules and rebuild

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Statikus fájlok kiszolgálása a Vite buildelt mappából
+// Statikus fájlok kiszolgálása a dist mappából
 app.use(express.static(path.join(__dirname, "dist")));
-app.use(cors());
 
-// Minden más útvonalra az index.html-t adjuk vissza
+// Minden más útvonalra a React app index.html-t küldje
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Szerver fut a következő porton: http://localhost:${PORT}`);
+  console.log(`Szerver fut a ${PORT} porton`);
 });
